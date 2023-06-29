8c39e77313d360642ced5f1975624c337e201f20
JiangHao <sukhao@163.com>
2023-06-29 12:13:56 +0000

    update lovers-server/src/main/java/loversmission/hoodee/config/HttpsConfiguration.java.
    Signed-off-by: JiangHao <sukhao@163.com>

--- lovers-server/src/main/java/loversmission/hoodee/config/HttpsConfiguration.java
+++ lovers-server/src/main/java/loversmission/hoodee/config/HttpsConfiguration.java
@@ -1,53 +1,54 @@
-package loversmission.hoodee.config;
+// package loversmission.hoodee.config;
 
 
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.apache.catalina.Context;
+// import org.apache.catalina.connector.Connector;
+// import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+// import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+// import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-/**
- * @version 1.0
- * @author: jianghao
- * @createTime: 2022年07月29日 11:09
- */
-@Configuration
-public class HttpsConfiguration {
+// /**
+//  * http 8080 端口转发，后端配置https可以开启此配置
+//  * @version 1.0
+//  * @author: jianghao
+//  * @createTime: 2022年07月29日 11:09
+//  */
+// @Configuration
+// public class HttpsConfiguration {
 
-    @Value("${http-port}")
-    private int port;
+//     @Value("${http-port}")
+//     private int port;
 
-    @Value("${server.port}")
-    private int sslPort;
+//     @Value("${server.port}")
+//     private int sslPort;
 
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(redirectConnector());
-        return tomcat;
-    }
+//     @Bean
+//     public ServletWebServerFactory servletContainer() {
+//         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+//             @Override
+//             protected void postProcessContext(Context context) {
+//                 SecurityConstraint securityConstraint = new SecurityConstraint();
+//                 securityConstraint.setUserConstraint("CONFIDENTIAL");
+//                 SecurityCollection collection = new SecurityCollection();
+//                 collection.addPattern("/*");
+//                 securityConstraint.addCollection(collection);
+//                 context.addConstraint(securityConstraint);
+//             }
+//         };
+//         tomcat.addAdditionalTomcatConnectors(redirectConnector());
+//         return tomcat;
+//     }
 
-    private Connector redirectConnector() {
-        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-        connector.setScheme("http");
-        connector.setPort(port);
-        connector.setSecure(false);
-        connector.setRedirectPort(sslPort);
-        return connector;
-    }
-}
+//     private Connector redirectConnector() {
+//         Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+//         connector.setScheme("http");
+//         connector.setPort(port);
+//         connector.setSecure(false);
+//         connector.setRedirectPort(sslPort);
+//         return connector;
+//     }
+// }
